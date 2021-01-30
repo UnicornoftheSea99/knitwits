4bd03e7019ab644affbdd655f0867e1d36d0ab4b
lepidopterane-atsmith <sabowitz@smith.edu>
2021-01-29 21:23:39 -0500

    bare bones row counter screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+/* hell yeah. canyou see me typing here */
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Thneed Title Placeholder </h1>
+        <h2> dropdown for part, i am lazy and dont know the tags</h2>       
       </header>
+      <div>
+        <p>delete this p, but these thing should be left to right in here</p>
+        <p> - button replaces this </p>
+        <p> how many rows left to knit? number go here </p>
+        <p> + button replaces this </p>
+      </div>
+      <p> under the thingy goes optional stitch instructions whee </p>
     </div>
   );
 }
