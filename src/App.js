ab63a4dcc76301286ebfd6177a5c248937913fcb
UnicornoftheSea99 <erhyu@smith.edu>
2021-01-29 20:04:31 -0800

    added counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
 
 /* hell yeah. canyou see me typing here */
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Thneed Title Placeholder </h1>
-        <h2> dropdown for part, i am lazy and dont know the tags</h2>       
-      </header>
+      {/* <header className="App-header"> */}
+        <h1> KnitWits </h1>
+        {/* <h2> dropdown for part, i am lazy and dont know the tags</h2>        */}
+      {/* </header> */}
       <div>
-        <p>delete this p, but these thing should be left to right in here</p>
-        <p> - button replaces this </p>
-        <p> how many rows left to knit? number go here </p>
-        <p> + button replaces this </p>
+        <Counter />
       </div>
-      <p> under the thingy goes optional stitch instructions whee </p>
+      {/* <p> under the thingy goes optional stitch instructions whee </p> */}
     </div>
   );
 }
+}
+
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    left: 10
+  };
+  addClick = () => {
+    this.setState(({ count,left }) => ({
+      count: count + 1,
+      left: left -1
+    }));
+  };
+  minusClick = () => {
+    this.setState(({ count,left }) => ({
+      count: count - 1,
+      left: left +1
+    }));
+  };
+  render() {
+    const mystyle = {
+     display: "inline-block",
+    };
+    return (
+    <div style={mystyle}>
+      <button onClick={this.addClick}>+1</button>
+      <p>Rows Completed: {this.state.count}</p>
+      <p>Rows Left: {this.state.left}</p>
+      <button onClick={this.minusClick}>-1</button>
+    </div>
+    )
+  }
+}
+
+
+
 
 export default App;
