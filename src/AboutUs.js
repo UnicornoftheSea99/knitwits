94feb14f7a9c78b606e0e97f6fa472f0a14ea5cf
UnicornoftheSea99 <erhyu@smith.edu>
2021-01-30 18:16:11 -0800

    finish about us

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -8,7 +8,8 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import Grid from '@material-ui/core/Grid';
+import Container from "@material-ui/core/Container";
 
 
   
@@ -16,86 +17,102 @@ import Typography from '@material-ui/core/Typography';
 class AboutUs extends Component{ 
   
   render() {
-    const useStyles = makeStyles({
-      root: {
-        maxWidth: 345,
-      },
-      media: {
-        height: 140,
-      },
-    });
+    
       return (
         <div>
+           <Container>
           <h2>About Us</h2>
           <p>Our names are Emily, Sarah, and Ananda and we are the Low Key Influencers! We
             made this project KnitWits as a nod to a hobby all three of us have leaned on during 
             quarantine, crocheting and knitting.
           </p>
-          <Card
-      style={{
-        maxWidth: 345,
-        boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
-        backgroundColor: "#fafafa",
-      }}
-    >
-      <CardMedia style={{ height: "300px" }} image={'knitscarf.jpg'} />
-    </Card>
-          {/* <Card className={useStyles.root}>
-          <CardActionArea>
-            <CardMedia
-              className={useStyles.media}
-              image={"./knit-plush.jpg"}
-              title="Contemplative Reptile"
-            />
+          <Grid container>
+            <Grid item xs={4}>
+            
+             <Card
+            style={{
+              maxWidth: 345,
+              boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+              backgroundColor: "#fafafa",
+            }}
+          >
+            <CardMedia style={{ height: "300px" }} image={'emily.jpg'} />
             <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
+              {/* <Typography gutterBottom variant="h5" component="h3"> */}
+              <Typography component="h3">
                 Emily
               </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                across all continents except Antarctica
-              </Typography>
+                Computer Science and French Major 
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                Smith College '21
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                Hobbies: Crocheting Amigurumis!
+                </Typography>
             </CardContent>
-          </CardActionArea>
-        </Card> */}
+          </Card>
+          </Grid>
 
-        {/* <Card className={useStyles.root}>
-          <CardActionArea>
-            <CardMedia
-              className={useStyles.media}
-              image="./knit-plush.jpg"
-              title="Contemplative Reptile"
-            />
+          <Grid item xs={4}>
+          <Card
+            style={{
+              maxWidth: 345,
+              boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+              backgroundColor: "#fafafa",
+            }}
+          >
+            <CardMedia style={{ height: "300px" }} image={'ananda.jpg'} />
             <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
+              {/* <Typography gutterBottom variant="h5" component="h3"> */}
+              <Typography component="h3">
                 Ananda
               </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                across all continents except Antarctica
-              </Typography>
+                Computer Science and Statistical and Data Sciences
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                Smith College '22
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                Hobbies: Knitting for Friends!
+                </Typography>
             </CardContent>
-          </CardActionArea>
-        </Card>
+          </Card>
+            </Grid>
 
-        <Card className={useStyles.root}>
-          <CardActionArea>
-            <CardMedia
-              className={useStyles.media}
-              image="./knit-plush.jpg"
-              title="Contemplative Reptile"
-            />
+            <Grid item xs={4}>
+          <Card
+            style={{
+              maxWidth: 345,
+              boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+              backgroundColor: "#fafafa",
+            }}
+          >
+            <CardMedia style={{ height: "300px"}} image={'sarah.PNG'} />
             <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
+              {/* <Typography gutterBottom variant="h5" component="h3"> */}
+              <Typography component="h3">
                 Sarah
               </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                across all continents except Antarctica
-              </Typography>
+                Computer Science and Classics
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                Smith College '20
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                Hobbies: Knitting Complex Math Patterns!
+                </Typography>
             </CardContent>
-          </CardActionArea>
-        </Card> */}
+          </Card>
+            </Grid>
+          </Grid>
+          
+          
+          
+          </Container>
        </div>
 
     );
